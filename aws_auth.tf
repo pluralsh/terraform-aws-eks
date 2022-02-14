72638878168ca8ee66532b85cd18e865ed48c63b
DavidSpek <vanderspek.david@gmail.com>
2022-02-14 14:11:38 +0100

    cleanup

--- aws_auth.tf
+++ aws_auth.tf
@@ -1,15 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-# output "aws_auth_roles" {
-#   description = "Roles for use in aws-auth ConfigMap"
-#   value = [
-#     {
-#       worker_role_arn = coalescelist(aws_iam_role.workers[*].arn, [""])[0]
-#       platform        = "linux"
-#     }
-#   ]
-# }
-
 locals {
   auth_launch_template_worker_roles = [
     for index in range(0, var.create_eks ? local.worker_group_launch_template_count : 0) : {
